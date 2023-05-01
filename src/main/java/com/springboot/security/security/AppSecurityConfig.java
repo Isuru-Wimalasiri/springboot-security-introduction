cf01955896beaa592bb957f363d19f03e78ca915
Isuru Wimalasiri <isuru.wimalasiri1997@gmail.com>
2023-05-01 17:13:07 +0530

    learned 1st step of authentication providers .

--- src/main/java/com/springboot/security/security/AppSecurityConfig.java
+++ src/main/java/com/springboot/security/security/AppSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.springboot.security.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,5 +19,14 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin().and()
                 .httpBasic();
+    }
+
+    // Inmemory authentication provider.
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}123456")
+                .authorities("USER");
     }
 }
